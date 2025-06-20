be38e3fb26fca4414a26259b80ed52894f008fc1
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-06-20 21:10:56 +0700

    ui: layout grid messsage page

--- src/app/(dashboard)/dashboard/message/page.tsx
+++ src/app/(dashboard)/dashboard/message/page.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function MessagePage() {
   return (
-    <div>
-      <p className='text-3xl text-white'>this is message page</p>
+    <div className='flex items-stretch gap-4'>
+      <div className='w-80 h-full bg-white rounded-2xl py-5 px-3 min-h-screen'></div>
+      <div className='flex-1 h-full bg-white rounded-2xl p-7 min-h-screen'></div>
     </div>
   );
 }
