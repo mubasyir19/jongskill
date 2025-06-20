12f7da070fed5242e49984b1956d58d65d45036f
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-06-20 21:25:44 +0700

    ui: adjustment styling sidebar

--- src/components/organism/Sidebar.tsx
+++ src/components/organism/Sidebar.tsx
@@ -8,21 +8,21 @@ import React from 'react';
 export default function Sidebar() {
   const pathname = usePathname();
   return (
-    <aside className='hidden w-40 lg:w-40 xl:w-72 py-6 lg:flex flex-col justify-between'>
+    <aside className='hidden w-32 lg:w-40 xl:w-64 py-6 lg:flex flex-col justify-between'>
       <div className='mt-9'>
         <div className='flex flex-col xl:flex-row justify-center items-center gap-2'>
-          <Image src={'/images/logo.png'} width={280} height={280} alt='logo' className='size-16' />
+          <Image src={'/images/logo.png'} width={280} height={280} alt='logo' className='size-12 xl:size-16' />
           <div className=''>
-            <h3 className='text-base text-center xl:text-start md:text-lg lg:text-xl xl:text-2xl font-bold text-[var(--color-primary)]'>
+            <h3 className='text-base text-center xl:text-start lg:text-lg xl:text-xl font-bold text-[var(--color-primary)]'>
               My<span className='mt-1 text-white'>Skill</span>
             </h3>
-            <p className='text-xs text-center xl:text-start text-white/70'>Skill Up, Ignite Change</p>
+            <p className='text-[8px] xl:text-xs text-center xl:text-start text-white/70'>Skill Up, Ignite Change</p>
           </div>
         </div>
         <div className='flex flex-col gap-4 pl-2 lg:pl-3 xl:pl-6 mt-10'>
           <Link
             href={`/dashboard`}
-            className={`text-white text-xs md:text-sm lg:text-base xl:text-xl font-semibold rounded-l-full flex items-center gap-2 lg:gap-3 xl:gap-6 py-2 lg:py-3 xl:py-5 pl-2 lg:pl-3 xl:pl-5 ${
+            className={`text-white text-xs md:text-sm lg:text-base xl:text-lg font-semibold rounded-l-full flex items-center gap-2 lg:gap-3 xl:gap-4 py-2 lg:py-3 pl-2 lg:pl-3 xl:pl-5 ${
               pathname === '/dashboard' ? 'bg-[var(--color-primary)]' : 'bg-none'
             }`}
           >
@@ -31,7 +31,7 @@ export default function Sidebar() {
           </Link>
           <Link
             href={`/dashboard/course`}
-            className={`text-white text-xs md:text-sm lg:text-base xl:text-xl font-semibold rounded-l-full flex items-center gap-2 lg:gap-3 xl:gap-6 py-2 lg:py-3 xl:py-5 pl-2 lg:pl-3 xl:pl-5 hover:bg-[var(--color-primary)] transition-all duration- ${
+            className={`text-white text-xs md:text-sm lg:text-base xl:text-lg font-semibold rounded-l-full flex items-center gap-2 lg:gap-3 xl:gap-4 py-2 lg:py-3 pl-2 lg:pl-3 xl:pl-5 hover:bg-[var(--color-primary)] transition-all duration- ${
               pathname === '/dashboard/course' ? 'bg-[var(--color-primary)]' : 'bg-none'
             }`}
           >
@@ -40,7 +40,7 @@ export default function Sidebar() {
           </Link>
           <Link
             href={`/dashboard/message`}
-            className={`text-white text-xs md:text-sm lg:text-base xl:text-xl font-semibold rounded-l-full flex items-center gap-2 lg:gap-3 xl:gap-6 py-2 lg:py-3 xl:py-5 pl-2 lg:pl-3 xl:pl-5 hover:bg-[var(--color-primary)] transition-all duration-100 ${
+            className={`text-white text-xs md:text-sm lg:text-base xl:text-lg font-semibold rounded-l-full flex items-center gap-2 lg:gap-3 xl:gap-4 py-2 lg:py-3 pl-2 lg:pl-3 xl:pl-5 hover:bg-[var(--color-primary)] transition-all duration-100 ${
               pathname === '/dashboard/message' ? 'bg-[var(--color-primary)]' : 'bg-none'
             }`}
           >
