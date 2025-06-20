11a3981d95a7f644dac87a2c95cafa13c97c1de7
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-06-20 13:18:03 +0700

    update: error handling input

--- src/components/molecules/InputField.tsx
+++ src/components/molecules/InputField.tsx
@@ -8,9 +8,10 @@ interface InputFieldProps {
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
+  error: string;
 }
 
-export default function InputField({ label, type, name, value, placeholder, onChange }: InputFieldProps) {
+export default function InputField({ label, type, name, value, placeholder, onChange, error }: InputFieldProps) {
   return (
     <div className='flex flex-col gap-1'>
       <label htmlFor={name} className='text-black'>
@@ -22,7 +23,11 @@ export default function InputField({ label, type, name, value, placeholder, onCh
         value={value}
         placeholder={placeholder}
         onChange={onChange}
-        className='rounded-lg border border-[var(--color-shades-800)] bg-secondary-1 p-4 text-[var(--color-shades-900)] placeholder:text-[var(--color-shades-800)] focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]'
+        className={`rounded-lg border p-4 text-[var(--color-shades-900)] placeholder:text-[var(--color-shades-800)] focus:outline-none ${
+          error
+            ? 'border-[var(--color-error)] focus:ring-[var(--color-error)] focus:border-[var(--color-error)]'
+            : 'border-[var(--color-shades-800)] focus:ring-2 focus:ring-[var(--color-primary)]'
+        }`}
       />
     </div>
   );
